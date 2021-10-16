87b8b497f405767e5242ea03959db3d91bc41092
jibinmathew69 <jibinmathew.69@gmail.com>
2021-10-17 00:23:29 +0700

    Add door-policy

---
+++ poetry-club-door-policy/door-policy.js
@@ -0,0 +1,17 @@
+const POLITE_WORD = ', please'
+
+export function frontDoorResponse(line) {
+	return line[0];
+}
+
+export function frontDoorPassword(word) {
+	return word[0].toUpperCase()+word.slice(1).toLowerCase();
+}
+
+export function backDoorResponse(line) {
+	return line.trim().slice(-1);
+}
+
+export function backDoorPassword(word) {
+	return frontDoorPassword(word)+POLITE_WORD;
+}
