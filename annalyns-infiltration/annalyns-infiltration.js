31ae412c1faabf463b3ce8b50b3492a10df26595
jibinmathew69 <jibinmathew.69@gmail.com>
2021-10-18 18:44:13 +0700

    Add annalyns-infilteration

---
+++ annalyns-infiltration/annalyns-infiltration.js
@@ -0,0 +1,20 @@
+export function canExecuteFastAttack(knightIsAwake) {
+	return !knightIsAwake;
+}
+
+export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
+	return knightIsAwake || archerIsAwake || prisonerIsAwake;
+}
+
+export function canSignalPrisoner(archerIsAwake, prisonerIsAwake) {
+	return !archerIsAwake && prisonerIsAwake;
+}
+
+export function canFreePrisoner(
+  knightIsAwake,
+  archerIsAwake,
+  prisonerIsAwake,
+  petDogIsPresent
+) {
+	return (petDogIsPresent && !archerIsAwake) || (prisonerIsAwake && (!knightIsAwake && !archerIsAwake));
+}
