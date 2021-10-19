3d849132db99b3caeaf4dd4091590cfed50f817b
jibinmathew69 <jibinmathew.69@gmail.com>
2021-10-20 00:39:32 +0700

    Add elyses-enchantments

---
+++ elyses-enchantments/enchantments.js
@@ -0,0 +1,78 @@
+export function getItem(cards, position) {
+	return cards[position];
+}
+
+export function setItem(cards, position, replacementCard) {
+	cards[position] = replacementCard;
+	return cards;
+}
+
+/**
+ * Insert newCard at the end of the cards array
+ *
+ * @param {number[]} cards
+ * @param {number} newCard
+ *
+ * @returns {number[]} the cards with the newCard applied
+ */
+export function insertItemAtTop(cards, newCard) {
+  throw new Error('Implement the insertItemAtTop function');
+}
+
+/**
+ * Remove the card at the 0-based position
+ *
+ * @param {number[]} cards
+ * @param {number} position
+ *
+ * @returns {number[]} the cards without the removed card
+ */
+export function removeItem(cards, position) {
+  throw new Error('Implement the removeItem function');
+}
+
+/**
+ * Remove card from the end of the cards array
+ *
+ * @param {number[]} cards
+ *
+ * @returns {number[]} the cards without the removed card
+ */
+export function removeItemFromTop(cards) {
+  throw new Error('Implement the removeItemFromTop function');
+}
+
+/**
+ * Insert newCard at beginning of the cards array
+ *
+ * @param {number[]} cards
+ * @param {number} newCard
+ *
+ * @returns {number[]} the cards including the new card
+ */
+export function insertItemAtBottom(cards, newCard) {
+  throw new Error('Implement the insertItemAtBottom function');
+}
+
+/**
+ * Remove card from the beginning of the cards cards
+ *
+ * @param {number[]} cards
+ *
+ * @returns {number[]} the cards without the removed card
+ */
+export function removeItemAtBottom(cards) {
+  throw new Error('Implement the removeItemAtBottom function');
+}
+
+/**
+ * Compare the number of cards with the given stackSize
+ *
+ * @param {number[]} cards
+ * @param {number} stackSize
+ *
+ * @returns {boolean} true if there are exactly stackSize number of cards, false otherwise
+ */
+export function checkSizeOfStack(cards, stackSize) {
+  throw new Error('Implement the checkSizeOfStack function');
+}
