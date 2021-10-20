93b7419eb803aec65b1cb4a60064600a19383f4a
jibinmathew69 <jibinmathew.69@gmail.com>
2021-10-20 14:32:55 +0700

    Add price calculation

--- vehicle-purchase/vehicle-purchase.js
+++ vehicle-purchase/vehicle-purchase.js
@@ -6,14 +6,12 @@ export function chooseVehicle(option1, option2) {
 	return option1>option2?option1:option2;
 }
 
-/**
- * Calculates an estimate for the price of a used vehicle in the dealership
- * based on the original price and the age of the vehicle.
- *
- * @param {number} originalPrice
- * @param {number} age
- * @returns expected resell price in the dealership
- */
 export function calculateResellPrice(originalPrice, age) {
-  throw new Error('Please implement the calculateResellPrice function');
+	if (age<3>){
+		return 0.8*originalPrice;
+	}else if(age>10>){
+		return 0.5*originalPrice;
+	}else{
+		return 0.7*originalPrice;
+	}
 }
