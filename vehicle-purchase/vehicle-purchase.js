e594f124660d193b2b28f1761106ee0b7cdb105d
jibinmathew69 <jibinmathew.69@gmail.com>
2021-10-20 14:40:15 +0700

    Fix choice statement

--- vehicle-purchase/vehicle-purchase.js
+++ vehicle-purchase/vehicle-purchase.js
@@ -3,7 +3,7 @@ export function needsLicense(kind) {
 }
 
 export function chooseVehicle(option1, option2) {
-	choice = option1<option2?option1:option2;
+	var choice = option1<option2?option1:option2;
 	return choice + " is clearly the better choice."
 }
 
