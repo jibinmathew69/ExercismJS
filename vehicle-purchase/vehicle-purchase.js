13194e9af69cd7f6a102f769c5f137e0615d7085
jibinmathew69 <jibinmathew.69@gmail.com>
2021-10-20 14:35:17 +0700

    Fix price calculation

--- vehicle-purchase/vehicle-purchase.js
+++ vehicle-purchase/vehicle-purchase.js
@@ -7,9 +7,9 @@ export function chooseVehicle(option1, option2) {
 }
 
 export function calculateResellPrice(originalPrice, age) {
-	if (age<3>){
+	if (age<3){
 		return 0.8*originalPrice;
-	}else if(age>10>){
+	}else if(age>10){
 		return 0.5*originalPrice;
 	}else{
 		return 0.7*originalPrice;
