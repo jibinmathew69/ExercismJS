fbaac9b777cfeae22d8047a255cd229d6be07014
jibinmathew69 <jibinmathew.69@gmail.com>
2021-10-21 19:45:25 +0700

    Add lime count

--- mixed-juices/mixed-juices.js
+++ mixed-juices/mixed-juices.js
@@ -5,12 +5,24 @@ const DRINK_TIME = {
 	'Tropical Island': 3,
 	'All or Nothing': 3,
 }
+
+const WEDGES = {
+	'small': 6,
+	'medium': 8,
+	'large': 10,
+}
+
 export function timeToMixJuice(name) {
 	return DRINK_TIME[name] || 2.5;
 }
 
 export function limesToCut(wedgesNeeded, limes) {
-  throw new Error('Please implement the limesToCut function');
+	let count = 0;
+	while (wedgesNeeded>0 && count<limes.length){
+		wedgesNeeded -= WEDGES[limes[count]]
+		count += 1;
+	}
+	return count;
 }
 
 export function remainingOrders(timeLeft, orders) {
